be5d3394cbfbe020cb8f3592bced3bd03d86db1f
Serenity <d.palbanese@gmail.com>
2020-01-20 20:42:21 +0100

    Added auth guard

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,11 +2,24 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
 import {LoginComponent} from './login/login.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AuthGuard} from '../guards/auth.guard';
 
 export const appRoutes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+      path: '',
+      runGuardsAndResolvers: 'always',
+      canActivate: [AuthGuard],
+      children: [
+          {
+              path: 'dashboard',
+              component: DashboardComponent
+          }
+      ]
     },
     {
         path: 'register',
